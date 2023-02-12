c96cea69d0427943c155eac0cb8877facfea5215
venomega <guilleps92@gmail.com>
2023-02-11 20:37:08 -0500

    added 2

--- gui/2/main.go
+++ gui/2/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	app := app.New()
 	root := app.NewWindow("The title")
+	root.SetContent(widget.NewButton("HIT ME", func() { println("ASDASDASD") }))
 	root.ShowAndRun()
 }
